11effa01a08e15688c3793fa088b9d2a44a75c55
Younes HENNI <youn.henni@gmail.com>
2024-09-28 16:08:56 +0100

    feat(index): create http server
    Removes the console.log statement. Initiates an HTTP server listening on port 3000. HTTP server replies to all requests with 200 OK with a body of "Hello Worlds"
    
    BREAKING CHANGE: No longer logs package.json

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import fs from "fs";
-const fileContent = fs.readFileSync("./package.json");
-console.log(fileContent.toString());
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello Worlds");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
