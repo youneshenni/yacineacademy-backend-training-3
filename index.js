0b37dd2be126a37033e77830cbd84269ddd5b88b
Younes HENNI <youn.henni@gmail.com>
2024-09-28 17:05:23 +0100

    feat(express): adds express.js
    configures express.js. Adds static page. Adds dynamic page showing the IP address of the client

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-import http from "http";
+import express from "express";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello Worlds");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
 });
 
-server.listen(3001, () => {
-  console.log("Server is running on port 3000");
+app.get("/about", (req, res) => {
+  res.status(200).send("About Us");
+});
+
+app.post("/contact", (req, res) => {
+  res.status(200).send("Contact Us");
+});
+
+app.get("/page1.html", (req, res) => {
+  res
+    .status(200)
+    .send("Page 1" + req.socket.remoteAddress + req.socket.remoteFamily);
+});
+
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
