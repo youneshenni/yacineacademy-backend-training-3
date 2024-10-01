34a19cb2f8a3fd35fbc4b12d12e56b072f69f05b
ihab <ihabkenouze11@gmail.com>
2024-10-01 19:15:00 +0000

    fix(users): adds info page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { appendFileSync, existsSync, writeFileSync } from "fs";
+import { appendFileSync, existsSync, readFileSync, writeFileSync } from "fs";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
@@ -21,26 +23,27 @@ app.get("/page1.html", (req, res) => {
   res.status(200).send(req.socket.remoteAddress);
 });
 
-
 app.post("/register", (req, res) => {
   console.log(req.body);
   if (!existsSync("./data.csv"))
-    writeFileSync("./data.csv", "Nom,Prénom,Email\n");
+    writeFileSync("./data.csv", "Nom,Prénom,Email");
   appendFileSync(
     "./data.csv",
-    `${req.body.nom},${req.body.prenom},${req.body.email}\n`
+    `\n${req.body.nom},${req.body.prenom},${req.body.email}`
   );
-  res.status(200).send("User registered successfully");
+  res.status(200).render("info", req.body);
 });
-app.post("/register", (req, res) => {
-  console.log(req.body);
-  if (!existsSync("./data.csv"))
-    writeFileSync("./data.csv", "Nom,Prénom,Email\n");
-  appendFileSync(
-    "./data.csv",
-    `${req.body.nom},${req.body.prenom},${req.body.email}\n`
-  );
-  res.status(200).send("User registered successfully");
+
+app.get("/users", (req, res) => {
+  const csvBuffer = readFileSync("./data.csv");
+  const users =
+    csvBuffer
+      .toString()
+      .split("\n")
+      .slice(1)
+      .map((row) => row.split(","))
+      .map(([nom, prenom, email]) => ({ nom, prenom, email }))
+  res.status(200).render("users", { users });
 });
 
 app.use(express.static("public"));
