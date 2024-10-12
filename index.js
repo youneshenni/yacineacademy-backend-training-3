61c80b14de2a16ee2bf028af90f83c9e7a4893ee
Younes HENNI <youn.henni@gmail.com>
2024-10-12 16:26:39 +0000

    feat(language): set language using Cookies

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from "express";
 import userRouter from "./user/api.js";
 import morganMiddleware from "./util/log/morgan.js";
 import Logger from "./util/log/winston.js";
+import cookieParser from 'cookie-parser';
+import { languageMiddleware, langaugeEndpoint } from "./middleware/langauge.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(languageMiddleware)
+
+app.post('/language', langaugeEndpoint);
 
 app.use(morganMiddleware)
 
---
+++ middleware/langauge.js
@@ -0,0 +1,19 @@
+export function languageMiddleware(req, res, next) {
+    if (req.cookies.language) {
+        req.language = req.cookies.language;
+    } else {
+        const lang = req.headers['accept-language'];
+        if (lang) {
+            const [language] = lang.split(',');
+            req.language = language;
+        }
+        else req.language = 'en';
+    }
+    res.cookie('language', req.language);
+    next();
+}
+
+export function langaugeEndpoint(req, res) {
+    res.cookie('language', req.body.language);
+    res.status(201).send("Created");
+}
